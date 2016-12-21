apache/flink

[FLINK-4657] Implement HighAvailabilityServices based on ZooKeeper
[FLINK-4657] Implement a few rpc calls for JobMaster

[FLINK-4657][cluster management] Address review comments

[FLINK-4657][cluster management] Throw exception when error occurred when request input split

flink-runtime/src/main/java/org/apache/flink/runtime/jobmaster/JobMasterGateway.java
@@ -19,7 +19,15 @@
 package org.apache.flink.runtime.jobmaster;
 
 import org.apache.flink.runtime.concurrent.Future;
-import org.apache.flink.runtime.messages.Acknowledge;
+import org.apache.flink.runtime.executiongraph.ExecutionAttemptID;
+import org.apache.flink.runtime.executiongraph.ExecutionJobVertex;
+import org.apache.flink.runtime.executiongraph.ExecutionVertex;
+import org.apache.flink.runtime.io.network.partition.ResultPartition;
+import org.apache.flink.runtime.io.network.partition.ResultPartitionID;
+import org.apache.flink.runtime.jobgraph.IntermediateDataSetID;
+import org.apache.flink.runtime.jobgraph.JobVertexID;
+import org.apache.flink.runtime.jobmaster.message.NextInputSplit;
+import org.apache.flink.runtime.jobmaster.message.PartitionState;
 import org.apache.flink.runtime.rpc.RpcGateway;
 import org.apache.flink.runtime.taskmanager.TaskExecutionState;
 
@@ -47,7 +55,47 @@ public interface JobMasterGateway extends RpcGateway {
 	 * Updates the task execution state for a given task.
 	 *
 	 * @param taskExecutionState New task execution state for a given task
-	 * @return Future acknowledge of the task execution state update
+	 * @return Future flag of the task execution state update result
 	 */
-	Future<Acknowledge> updateTaskExecutionState(TaskExecutionState taskExecutionState);
+	Future<Boolean> updateTaskExecutionState(TaskExecutionState taskExecutionState);
+
+	/**
+	 * Requesting next input split for the {@link ExecutionJobVertex}. The next input split is sent back to the sender
+	 * as a {@link NextInputSplit} message.
+	 *
+	 * @param vertexID         The job vertex id
+	 * @param executionAttempt The execution attempt id
+	 * @return The future of the input split. If there is no further input split, will return an empty object.
+	 * @throws Exception if some error occurred or information mismatch.
+	 */
+	Future<NextInputSplit> requestNextInputSplit(
+		final JobVertexID vertexID,
+		final ExecutionAttemptID executionAttempt) throws Exception;
+
+	/**
+	 * Requests the current state of the partition.
+	 * The state of a partition is currently bound to the state of the producing execution.
+	 *
+	 * @param partitionId     The partition ID of the partition to request the state of.
+	 * @param taskExecutionId The execution attempt ID of the task requesting the partition state.
+	 * @param taskResultId    The input gate ID of the task requesting the partition state.
+	 * @return The future of the partition state
+	 */
+	Future<PartitionState> requestPartitionState(
+		final ResultPartitionID partitionId,
+		final ExecutionAttemptID taskExecutionId,
+		final IntermediateDataSetID taskResultId);
+
+	/**
+	 * Notifies the JobManager about available data for a produced partition.
+	 * <p>
+	 * There is a call to this method for each {@link ExecutionVertex} instance once per produced
+	 * {@link ResultPartition} instance, either when first producing data (for pipelined executions)
+	 * or when all data has been produced (for staged executions).
+	 * <p>
+	 * The JobManager then can decide when to schedule the partition consumers of the given session.
+	 *
+	 * @param partitionID The partition which has already produced data
+	 */
+	void scheduleOrUpdateConsumers(final ResultPartitionID partitionID);
 }

StephanEwen/incubator-flink

flink-runtime/src/main/java/org/apache/flink/runtime/jobmaster/JobMasterGateway.java
@@ -19,6 +19,15 @@
 package org.apache.flink.runtime.jobmaster;
 
 import org.apache.flink.runtime.concurrent.Future;
+import org.apache.flink.runtime.execution.ExecutionState;
+import org.apache.flink.runtime.executiongraph.ExecutionAttemptID;
+import org.apache.flink.runtime.executiongraph.ExecutionJobVertex;
+import org.apache.flink.runtime.executiongraph.ExecutionVertex;
+import org.apache.flink.runtime.io.network.partition.ResultPartition;
+import org.apache.flink.runtime.io.network.partition.ResultPartitionID;
+import org.apache.flink.runtime.jobgraph.IntermediateDataSetID;
+import org.apache.flink.runtime.jobgraph.JobVertexID;
+import org.apache.flink.runtime.jobmaster.message.NextInputSplit;
 import org.apache.flink.runtime.messages.Acknowledge;
 import org.apache.flink.runtime.rpc.RpcGateway;
 import org.apache.flink.runtime.taskmanager.TaskExecutionState;
@@ -47,7 +56,47 @@ public interface JobMasterGateway extends RpcGateway {
 	 * Updates the task execution state for a given task.
 	 *
 	 * @param taskExecutionState New task execution state for a given task
-	 * @return Future acknowledge of the task execution state update
+	 * @return Future flag of the task execution state update result
 	 */
-	Future<Acknowledge> updateTaskExecutionState(TaskExecutionState taskExecutionState);
+	Future<Boolean> updateTaskExecutionState(TaskExecutionState taskExecutionState);
+
+	/**
+	 * Requesting next input split for the {@link ExecutionJobVertex}. The next input split is sent back to the sender
+	 * as a {@link NextInputSplit} message.
+	 *
+	 * @param vertexID         The job vertex id
+	 * @param executionAttempt The execution attempt id
+	 * @return The future of the input split. If there is no further input split, will return an empty object.
+	 * @throws Exception if some error occurred or information mismatch.
+	 */
+	Future<NextInputSplit> requestNextInputSplit(
+		final JobVertexID vertexID,
+		final ExecutionAttemptID executionAttempt) throws Exception;
+
+	/**
+	 * Requests the current state of the partition.
+	 * The state of a partition is currently bound to the state of the producing execution.
+	 *
+	 * @param partitionId     The partition ID of the partition to request the state of.
+	 * @param taskExecutionId The execution attempt ID of the task requesting the partition state.
+	 * @param taskResultId    The input gate ID of the task requesting the partition state.
+	 * @return The future of the partition state
+	 */
+	Future<ExecutionState> requestPartitionState(
+		final ResultPartitionID partitionId,
+		final ExecutionAttemptID taskExecutionId,
+		final IntermediateDataSetID taskResultId);
+
+	/**
+	 * Notifies the JobManager about available data for a produced partition.
+	 * <p>
+	 * There is a call to this method for each {@link ExecutionVertex} instance once per produced
+	 * {@link ResultPartition} instance, either when first producing data (for pipelined executions)
+	 * or when all data has been produced (for staged executions).
+	 * <p>
+	 * The JobManager then can decide when to schedule the partition consumers of the given session.
+	 *
+	 * @param partitionID The partition which has already produced data
+	 */
+	Future<Acknowledge> scheduleOrUpdateConsumers(final ResultPartitionID partitionID);
 }