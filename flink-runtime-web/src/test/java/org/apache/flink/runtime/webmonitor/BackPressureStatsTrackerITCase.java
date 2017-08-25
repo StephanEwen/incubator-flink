b82cee16ccb36c29e0673a2b3c00dbc412ed08e8
Nico Kruber <nico@data-artisans.com>
2017-08-25 09:49:44 +0200

    [FLINK-7514][tests] fix BackPressureStatsTrackerITCase releasing buffers twice

--- flink-runtime-web/src/test/java/org/apache/flink/runtime/webmonitor/BackPressureStatsTrackerITCase.java
+++ flink-runtime-web/src/test/java/org/apache/flink/runtime/webmonitor/BackPressureStatsTrackerITCase.java
@@ -222,6 +222,7 @@ public class BackPressureStatsTrackerITCase extends TestLogger {
 							//
 							for (Buffer buf : buffers) {
 								buf.recycle();
+								assertTrue(buf.isRecycled());
 							}
 
 							// Wait for all buffers to be available. The tasks
@@ -279,10 +280,6 @@ public class BackPressureStatsTrackerITCase extends TestLogger {
 				TestingUtils.stopActor(taskManager);
 
 				highAvailabilityServices.closeAndCleanupAllData();
-
-				for (Buffer buf : buffers) {
-					buf.recycle();
-				}
 
 				testBufferPool.lazyDestroy();
 			}
